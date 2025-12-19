c0f2bdc4e2df74260377ae75fe5e564871ffd996
Toby Inkster <mail@tobyinkster.co.uk>
2025-12-19 17:10:42 +0000

    bump version; changelog

--- lib/Class/XSConstructor.pm
+++ lib/Class/XSConstructor.pm
@@ -9,7 +9,7 @@ use List::Util 1.45 qw( uniq );
 
 BEGIN {
 	our $AUTHORITY = 'cpan:TOBYINK';
-	our $VERSION   = '0.014001';
+	our $VERSION   = '0.015000';
 	
 	if ( eval { require Types::Standard; 1 } ) {
 		Types::Standard->import(
