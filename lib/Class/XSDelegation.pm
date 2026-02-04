b413d758cc026ff3f232699851fbf2558dda907c
Toby Inkster <mail@tobyinkster.co.uk>
2026-02-04 23:28:34 +0000

    bump version; changelog

--- lib/Class/XSDelegation.pm
+++ lib/Class/XSDelegation.pm
@@ -6,7 +6,7 @@ use Class::XSConstructor ();
 package Class::XSDelegation;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.023004';
+our $VERSION   = '0.023005';
 
 sub import {
 	my $class = shift;
